0cb7bca1e897196b38f84ceb789eff8f33da300b
kiran <kiranrajbaburaj1992@gmail.com>
2018-12-19 23:15:44 +0530

    Add project files.

---
+++ AndroidApp/MainActivity.cs
@@ -0,0 +1,88 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using System.IO;
+using System;
+using Android.Content.Res;
+using System.Security.Cryptography;
+
+namespace AndroidApp
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+
+            string publicKey;
+            string privateKey;
+
+            AssetManager assets = this.Assets;
+            using (StreamReader sr = new StreamReader(assets.Open("publicKey.pem")))
+            {
+                publicKey = sr.ReadToEnd();
+            }
+            using (StreamReader sr = new StreamReader(assets.Open("privateKey.pem")))
+            {
+                privateKey = sr.ReadToEnd();
+            }
+
+            //publicKey = File.ReadAllText(@"D://publicKey.pem");
+            //privateKey = File.ReadAllText(@"D://privateKey.pem");
+
+
+            EditText editText = (EditText)FindViewById(Resource.Id.inputEt);
+            Button button = (Button)FindViewById(Resource.Id.button1);
+
+            button.Click += delegate
+            {
+                string plain = editText.Text.ToString();
+                //string encrypted = RSAProvider.Encrypt(publicKey, plain);
+                //string decrypted = RSAProvider.Decrypt(privateKey, encrypted);
+
+
+
+                DateTime t1; //To use for timings
+
+                #region Load key from file and encrypt test
+                t1 = DateTime.Now;
+                Console.WriteLine("*** Load key from file and encrypt test ***");
+                Console.WriteLine("Loading premade private keys..");
+                //string loadedRSA = File.ReadAllText("keys/private.rsa.pem");
+                string loadedRSA = privateKey;
+                RSACryptoServiceProvider privateRSAkey = Crypto.Crypto.DecodeRsaPrivateKey(loadedRSA);
+
+                Console.WriteLine("Loading premade public key..");
+                //string loadedX509 = File.ReadAllText("keys/public.x509.pem");
+                string loadedX509 = publicKey;
+                RSACryptoServiceProvider publicX509key = Crypto.Crypto.DecodeX509PublicKey(loadedX509);
+
+                string secret = "Hello world - 1";
+                Console.WriteLine("Using public key, encrypt \"" + secret + "\"..");
+                string supersecret = Crypto.Crypto.EncryptString(secret, publicX509key);
+                Console.WriteLine("Encrypted: " + supersecret);
+
+                Console.WriteLine("Using private key, decrypt..");
+                string decodesecret = Crypto.Crypto.DecryptString(supersecret, privateRSAkey);
+                Console.WriteLine("Decrypted: " + decodesecret);
+                #endregion
+
+                editText.Text = decodesecret;
+            };
+
+        }
+    }
+}
+
+
+//http://travistidwell.com/blog/2013/09/06/an-online-rsa-public-and-private-key-generator/
+//http://travistidwell.com/jsencrypt/demo/
+//https://www.cnblogs.com/adylee/p/3611461.html
+
+//https://github.com/jrnker/CSharp-easy-RSA-PEM
---
+++ AndroidApp/RSAProvider.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace AndroidApp
+{
+    class RSAProvider
+    {
+        public static void GenerateKeys(out string PublicKey, out string PrivateKey)
+        {
+            using (RSACryptoServiceProvider RSA2048 = new RSACryptoServiceProvider(2048))
+            {
+                RSA2048.PersistKeyInCsp = false;
+                PublicKey = RSA2048.ToXmlString(false);
+                PrivateKey = RSA2048.ToXmlString(true);
+            }
+        }
+
+        public static string Encrypt(string PublicKey, string plain)
+        {
+            using (RSACryptoServiceProvider RSA2048 = new RSACryptoServiceProvider(2048))
+            {
+                RSA2048.PersistKeyInCsp = false;
+                RSA2048.LoadPublicKeyPEM(PublicKey);
+                //RSA2048.FromXmlString(PublicKey);
+                return Convert.ToBase64String(RSA2048.Encrypt(Encoding.UTF8.GetBytes(plain), false));
+            }
+        }
+
+        public static string Decrypt(string PrivateKey, string cipher)
+        {
+            using (RSACryptoServiceProvider RSA2048 = new RSACryptoServiceProvider(2048))
+            {
+                RSA2048.PersistKeyInCsp = false;
+                RSA2048.LoadPrivateKeyPEM(PrivateKey);
+                //RSA2048.FromXmlString(PrivateKey);
+                return Encoding.UTF8.GetString(RSA2048.Decrypt(Convert.FromBase64String(cipher), false));
+            }
+        }
+    }
+}
